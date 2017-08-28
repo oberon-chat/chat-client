cd7326fe17cc70c739e34ad69bc6109ac3cbbe7b
Chris Laskey <contact@chrislaskey.com>
2017-08-28 19:42:40 -0400

    Render last activity time

---
+++ src/helpers/presence.js
@@ -0,0 +1,1 @@
+export const meta = (item, key) => ((item.metas || [])[0] || {})[key]
--- src/pages/Rooms/All.js
+++ src/pages/Rooms/All.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { map } from 'lodash'
+import moment from 'moment'
 import { joinRooms } from '../../actions/rooms'
 import { getRooms } from '../../reducers/rooms'
 import { getRoomsJoined } from '../../reducers/roomsJoined'
+import { meta } from '../../helpers/presence'
 import CreateRoomForm from './_Form'
+import { Icon } from 'antd'
 
 export class AllRooms extends Component {
   componentDidMount () {
@@ -15,17 +18,32 @@ export class AllRooms extends Component {
   render () {
     const { match, rooms, roomsJoined } = this.props
 
+    const renderRoom = (room, name) => {
+      const lastMessage = meta(room, "last_message")
+      const timestamp = lastMessage ? moment(lastMessage.timestamp).calendar() : null
+
+      return (
+        <li key={name}>
+          <Link to={match.url + '/' + name}>
+            <strong>{name}</strong>
+          </Link>
+          {' '}
+          {timestamp &&
+            <span>
+              <Icon type='clock-circle-o' />
+              {' '}
+              {timestamp}
+            </span>
+          }
+        </li>
+      )
+    }
+
     return (
       <div>
         <h1>Rooms</h1>
         <ul>
-          { map(rooms, (_, name) => (
-            <li key={name}>
-              <Link to={match.url + '/' + name}>
-                {name}
-              </Link>
-            </li>
-          )) }
+          { map(rooms, renderRoom) }
         </ul>
         <h3>Create Room</h3>
         <CreateRoomForm />
